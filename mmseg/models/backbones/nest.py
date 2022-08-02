0708dd810dee5dc9380b3b5567c8f9d443468f99
ngfuong <lenguyenphuong2204@gmail.com>
2022-08-02 17:19:44 +0700

    added nest backbone

--- mmseg/models/backbones/nest.py
+++ mmseg/models/backbones/nest.py
@@ -4,29 +4,54 @@ from functools import partial
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 from mmcv.utils import to_ntuple, to_2tuple
 # from mmcv.cnn.bricks import build_conv_layer
 from mmcv.cnn.bricks.drop import build_dropout
+from mmcv.runner import (BaseModule, CheckpointLoader, ModuleList,
+                         load_state_dict)
 # from mmcv.cnn.bricks.transformer import PatchEmbed
 
+from timm.models.layers import DropPath, to_2tuple, trunc_normal_, create_conv2d, create_pool2d, _assert
+from timm.models.helpers import named_apply
 from ..builder import BACKBONES
 # from mmcv.utils.conv import create_conv2d, create_conv2d_pad
 
-class ConvPool(nn.Module):
+#lass ConvPool(nn.Module):
+#   def __init__(self, in_channels, out_channels, norm_layer, pad_type=''):
+#       super().__init__()
+#       self.conv = nn.Conv2d(in_channels, out_channels, 3, padding=pad_type, bias=True)
+#       self.norm = norm_layer(out_channels)
+#       self.pool = nn.MaxPool2d(kernel_size=3, stride=2, padding=pad_type)
+#   
+#   def forward(self,x):
+#       assert(x.shape[-2]%2 == 0), 'BlockAgg requires even input spatial dims'
+#       assert(x.shape[-1]%2 == 0), 'BlockAgg requires even input spatial dims'
+#       x = self.conv(x)
+#       x = self.norm(x.permute(0,2,3,1)).permute(0,3,1,2)
+#       x = self.pool(x)
+#       return x    # (B,C,H//2,W//2)
+
+class ConvPool(BaseModule):
     def __init__(self, in_channels, out_channels, norm_layer, pad_type=''):
         super().__init__()
-        self.conv = nn.Conv2d(in_channels, out_channels, 3, padding=pad_type, bias=True)
+        self.conv = create_conv2d(in_channels, out_channels, kernel_size=3, padding=pad_type, bias=True)
         self.norm = norm_layer(out_channels)
-        self.pool = nn.MaxPool2d(kernel_size=3, stride=2, padding=pad_type)
-    
-    def forward(self,x):
-        assert(x.shape[-2]%2 == 0), 'BlockAgg requires even input spatial dims'
-        assert(x.shape[-1]%2 == 0), 'BlockAgg requires even input spatial dims'
+        self.pool = create_pool2d('max', kernel_size=3, stride=2, padding=pad_type)
+
+    def forward(self, x):
+        """
+        x is expected to have shape (B, C, H, W)
+        """
+        _assert(x.shape[-2] % 2 == 0, 'BlockAggregation requires even input spatial dims')
+        _assert(x.shape[-1] % 2 == 0, 'BlockAggregation requires even input spatial dims')
         x = self.conv(x)
-        x = self.norm(x.permute(0,2,3,1)).permute(0,3,1,2)
+        # Layer norm done over channel dim only
+        x = self.norm(x.permute(0, 2, 3, 1)).permute(0, 3, 1, 2)
         x = self.pool(x)
-        return x    # (B,C,H//2,W//2)
+        return x  # (B, C, H//2, W//2)
+
 
 def blockify(x, block_size:int):
     B, H, W, C  = x.shape
@@ -38,6 +63,7 @@ def blockify(x, block_size:int):
     x = x.transpose(2, 3).reshape(B, grid_height * grid_width, -1, C)
     return x  # (B, T, N, C)
 
+
 def deblockify(x, block_size: int):
     B, T, _, C = x.shape
     grid_size = int(math.sqrt(T))
@@ -47,7 +73,7 @@ def deblockify(x, block_size: int):
     return x  # (B, H, W, C)
 
 
-class Attention(nn.Module):
+class Attention(BaseModule):
     """
     This is much like `.vision_transformer.Attention` but uses *localised* self attention by accepting an input with
      an extra "image block" dim
@@ -83,7 +109,7 @@ class Attention(nn.Module):
         return x  # (B, T, N, C)
 
 
-class Mlp(nn.Module):
+class Mlp(BaseModule):
     """ MLP as used in Vision Transformer, MLP-Mixer and related networks
     """
     def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, bias=True, drop=0.):
@@ -108,7 +134,7 @@ class Mlp(nn.Module):
         return x
 
 
-class PatchEmbed(nn.Module):
+class PatchEmbed(BaseModule):
     """ 2D Image to Patch Embedding
     """
     def __init__(self, img_size=224, patch_size=16, in_chans=3, embed_dim=768, norm_layer=None, flatten=True):
@@ -132,17 +158,16 @@ class PatchEmbed(nn.Module):
         if self.flatten:
             x = x.flatten(2).transpose(1, 2)  # BCHW -> BNC
         x = self.norm(x)
-        
-        
         return x
-class TransformerLayer():
+
+
+class TransformerLayer(BaseModule):
     def __init__(self, dim, num_heads, mlp_ratio=4., qkv_bias=False, drop=0., attn_drop=0., drop_path=0.,
                  act_layer=nn.GELU, norm_layer=nn.LayerNorm):
         super().__init__()
         self.norm1 = norm_layer(dim)
         self.attn = Attention(dim, num_heads=num_heads, qkv_bias=qkv_bias, attn_drop=attn_drop, proj_drop=drop)
-        self.drop_path = build_dropout(
-            dict(type='DropPath', drop_prob=drop)) if drop_path > 0. else nn.Identity()
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
         self.norm2 = norm_layer(dim)
         mlp_hidden_dim = int(dim * mlp_ratio)
         self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
@@ -153,13 +178,27 @@ class TransformerLayer():
         x = x + self.drop_path(self.mlp(self.norm2(x)))
         return x
 
-class NestLevel(nn.Module):
+
+class NestLevel(BaseModule):
     """ Single hierarchical level of a Nested Transformer
     """
     def __init__(
-            self, num_blocks, block_size, seq_length, num_heads, depth, embed_dim, prev_embed_dim=None,
-            mlp_ratio=4., qkv_bias=True, drop_rate=0., attn_drop_rate=0., drop_path_rates=[],
-            norm_layer=None, act_layer=None, pad_type=''):
+            self, 
+            num_blocks, 
+            block_size, 
+            seq_length, 
+            num_heads, 
+            depth, 
+            embed_dim, 
+            prev_embed_dim=None,
+            mlp_ratio=4., 
+            qkv_bias=True, 
+            drop_rate=0.,
+            attn_drop_rate=0., 
+            drop_path_rates=[],
+            norm_layer=None, 
+            act_layer=None, 
+            pad_type=''):
         super().__init__()
         self.block_size = block_size
         self.grad_checkpointing = False
@@ -201,6 +240,11 @@ class NestLevel(nn.Module):
     
 @BACKBONES.register_module()
 class Nest(nn.Module):
+    """
+    Args:
+        frozen_stages (int): Stages to be frozen (stop grad and set eval mode).
+            -1 means not freezing any parameters
+    """
     def __init__(
             self, 
             img_size=224, 
@@ -218,27 +262,31 @@ class Nest(nn.Module):
             drop_path_rate=0.5, 
             norm_layer=None, 
             act_layer=None,
-            pad_type='', 
+            pad_type='same', 
             weight_init='', 
-            global_pool='avg'
+            global_pool='avg',
+            out_indices=(0,1,2,3),
+            frozen_stages=-1,
     ):
         for param_name in ['embed_dims', 'num_heads', 'depths']:
             param_value = locals()[param_name]
 
         embed_dims = to_ntuple(num_levels)(embed_dims)
         num_heads = to_ntuple(num_levels)(num_heads)
-        self.num_classes = num_classes
-        self.num_features = embed_dims[-1]
-        self.feature_info = []
-        norm_layer = norm_layer or partial(nn.LayerNorm, eps=1e-6)
-        act_layer = act_layer or nn.GELU
-        self.drop_rate = drop_rate
-        self.num_levels = num_levels
+
         if isinstance(img_size, collections.abc.Sequence):
             assert img_size[0] == img_size[1]
             img_size = img_size[0]
         assert img_size % patch_size == 0, '`patch_size` must divide `img_size` evenly'
         self.patch_size = patch_size
+        self.num_features = embed_dims[-1]
+        self.num_levels = num_levels
+        self.num_classes = num_classes
+        self.feature_info = []
+        self.drop_rate = drop_rate
+        norm_layer = norm_layer or partial(nn.LayerNorm, eps=1e-6)
+        act_layer = act_layer or nn.GELU
+        self.out_incides = out_indices
 
         # Num blocks per level
         self.num_blocks = (4 ** torch.arange(num_levels)).flip(0).tolist()
@@ -255,6 +303,8 @@ class Nest(nn.Module):
             img_size=img_size, patch_size=patch_size, in_chans=in_chans, embed_dim=embed_dims[0], flatten=False)
         self.num_patches = self.patch_embed.num_patches
         self.seq_length = self.num_patches // self.num_blocks[0]
+        # Added pos_drop after patch embedding
+        self.pos_drop = nn.Dropout(p=drop_rate)
 
         # Build up each hierarchical level
         levels = []
@@ -279,15 +329,71 @@ class Nest(nn.Module):
 
         self.init_weights(weight_init)
 
+    def forward_features(self, x):
+        x = self.patch_embed(x)
+        # Added forward pos_drop
+        x = self.pos_drop(x)
+        x = self.levels(x)
+        x = self.norm(x.permute(0, 2, 3, 1)).permute(0, 3, 1, 2)
+        return x
+        # return tuple(x) ???
+    
+    def forward_head(self, x, pre_logits: bool=False):
+        x = self.global_pool(x)
+        if self.drop_rate > 0.:
+            x = F.dropout(x, p=self.drop_rate, training=self.training)
+        return x if pre_logits else self.head(x)
 
     def forward(self, x):
-        pass
+        x = self.forward_features(x)
+        # x = self.forward_head(x)
+        return x
+    
+    @torch.jit.ignore
+    def init_weights(self, mode=''):
+        assert mode in ('nlhb', '')
+        head_bias = -math.log(self.num_classes) if 'nlhb' in mode else 0.
+        for level in self.levels:
+            trunc_normal_(level.pos_embed, std=.02, a=-2, b=2)
+        named_apply(partial(_init_nest_weights, head_bias=head_bias), self)
 
-    def init_weights(self, pretrained=None):
-        pass
+    # Added _freeze_stages according to swin transformer
+    def _freeze_stages(self):
+        if self.frozen_stages >= 0:
+            self.patch_embed.eval()
+            for param in self.patch_embed.parameters():
+                param.requires_grad = False
+        
+        if self.frozen_stages >= 2:
+            self.pos_drop.eval()
+            for i in range(0, self.frozen_stages - 1):
+                m = self.levels[i]
+                m.eval()
+                for param in m.parameters():
+                    param.requires_grad = False
+        
 
     def train(self, mode=True):
-        pass 
+        super(Nest, self).train(mode)
+        self._freeze_stages()
+
+def _init_nest_weights(module: nn.Module, name: str = '', head_bias: float = 0.):
+    """ NesT weight initialization
+    Can replicate Jax implementation. Otherwise follows vision_transformer.py
+    """
+    if isinstance(module, nn.Linear):
+        if name.startswith('head'):
+            trunc_normal_(module.weight, std=.02, a=-2, b=2)
+            nn.init.constant_(module.bias, head_bias)
+        else:
+            trunc_normal_(module.weight, std=.02, a=-2, b=2)
+            if module.bias is not None:
+                nn.init.zeros_(module.bias)
+    elif isinstance(module, nn.Conv2d):
+        trunc_normal_(module.weight, std=.02, a=-2, b=2)
+        if module.bias is not None:
+            nn.init.zeros_(module.bias)
+
 
 
 
